1af0ef8d51ee8278a8a01cc1f376b13cd970ac6c
Samialenzi21 <136755497+Samialenzi21@users.noreply.github.com>
2026-03-08 18:49:52 -0700

    Fix Supabase API key syntax error

--- src/supabase.js
+++ src/supabase.js
@@ -1,5 +1,5 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
 // ضع هنا بيانات مشروع Supabase الخاص بك
-const SUPABASE_URL = 'https://ggcswgxlbkeldpeuitce.supabase.co'const SUPABASE_ANON_KEY = ''sb_publishable_Xoq-s7IZFkiSu0UjyvEVAw_bCExY7I7'
+const SUPABASE_URL = 'https://ggcswgxlbkeldpeuitce.supabase.co'const SUPABASE_ANON_KEY = 'sb_publishable_Xoq-s7IZFkiSu0UjyvEVAw_bCExY7'
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
