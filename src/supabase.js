5567d5e42c26bd5b431f8cfb9462b65f5aba7e79
Samialenzi21 <136755497+Samialenzi21@users.noreply.github.com>
2026-03-08 18:52:47 -0700

    Update Supabase credentials format

--- src/supabase.js
+++ src/supabase.js
@@ -1,5 +1,7 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
-// ضع هنا بيانات مشروع Supabase الخاص بك
-const SUPABASE_URL = 'https://ggcswgxlbkeldpeuitce.supabase.co'const SUPABASE_ANON_KEY = 'sb_publishable_Xoq-s7IZFkiSu0UjyvEVAw_bCExY7'
+// الخاص بك Supabase ضع هنا بيانات مشروع
+const SUPABASE_URL = 'https://ggcswgxlbkeldpeuitce.supabase.co'
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdnY3N3Z3hsYmtlbGRwZXVpdGNlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA3OTkyMjAsImV4cCI6MjA1NjM3NTIyMH0.n5aTbWxDpNDBRqwNGfvWFb6kVZiQLlXjGTEqI6OjC6Q'
+
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
